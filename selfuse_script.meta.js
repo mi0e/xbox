2dfe92ccb5fee86e3c26aa2f7e43f5900d988217
mi0e <49353667+mi0e@users.noreply.github.com>
2025-05-21 22:04:00 +0800

    Create selfuse_script.meta.js

---
+++ selfuse_script.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Xbox 添加购物车
+// @version      2.1
+// @namespace    ...
+// @author       mi0e
+// @include      https://*.xbox.com/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @require https://scriptcat.org/lib/637/1.4.5/ajaxHooker.js#sha256=EGhGTDeet8zLCPnx8+72H15QYRfpTX4MbhyJ4lJZmyg=
+// @require https://cdn.jsdelivr.net/npm/sweetalert2@11
+// ==/UserScript==
