4c590846044a5694a39751835a5247e7229ae86e
mi0e <49353667+mi0e@users.noreply.github.com>
2025-05-21 14:59:56 +0800

    Create script.meta.js

---
+++ script.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Xbox 家庭组 尼区
+// @version      1.7
+// @namespace    ...
+// @author       erii
+// @include      https://*.xbox.com/*
+// @include      https://*.microsoft.com/*
+// @include      https://*.dynamics.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
+// @require      https://scriptcat.org/lib/637/1.4.5/ajaxHooker.js#sha256=EGhGTDeet8zLCPnx8+72H15QYRfpTX4MbhyJ4lJZmyg=
+// @updateURL    https://github.com/mi0e/xbox/script.meta.js
+// ==/UserScript==
