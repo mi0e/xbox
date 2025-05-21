6db58032ee69319653aa50dd5c8aad49a2ec9e21
mi0e <49353667+mi0e@users.noreply.github.com>
2025-05-21 15:18:38 +0800

    Create scriptDLC.meta.js

---
+++ scriptDLC.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         xbox 家庭组 DLC
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @match        *://*/*
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
+// ==/UserScript==
