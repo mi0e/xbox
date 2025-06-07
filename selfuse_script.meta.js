afddcf8e3d56701ccefe27e0f73964b7ec0ee9a7
mi0e <49353667+mi0e@users.noreply.github.com>
2025-06-07 18:04:08 +0800

    Update selfuse_script.meta.js

--- selfuse_script.meta.js
+++ selfuse_script.meta.js
@@ -10,3 +10,5 @@
 // @require https://scriptcat.org/lib/637/1.4.5/ajaxHooker.js#sha256=EGhGTDeet8zLCPnx8+72H15QYRfpTX4MbhyJ4lJZmyg=
 // @require https://cdn.jsdelivr.net/npm/sweetalert2@11
 // ==/UserScript==
+
+const disableUUIDs = ['123'];
