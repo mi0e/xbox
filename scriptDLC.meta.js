33d6a04fb1b42b47d6cad206386ad8100f24a06d
mi0e <49353667+mi0e@users.noreply.github.com>
2025-06-07 18:27:35 +0800

    Update scriptDLC.meta.js

--- scriptDLC.meta.js
+++ scriptDLC.meta.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         xbox 家庭组 DLC
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @match        *://*/*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
 // ==/UserScript==
+
+disableUUIDs = [];
